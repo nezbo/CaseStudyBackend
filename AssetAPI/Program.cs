0b6c62c792359d168a3608750bc451d0770d9edc
Emil <e.jacobsen@audensiel.com>
2025-05-28 14:20:47 -0400

    Fixing some issued that had arose in microservice version

--- AssetAPI/Program.cs
+++ AssetAPI/Program.cs
@@ -42,7 +42,7 @@ public class Program
             });
         }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
 
